aa59cd5a18f7c27ee03db4268b1f4c63bef24773
Hector Fernandes <hectorhu17@gmail.com>
2022-11-05 18:04:44 -0300

    feat: rota bustracker e index criados

--- app/bustracker/views.py
+++ app/bustracker/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import models
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	obj = models.Bus.objects.first()
+	field_names = [f.name for f in obj._meta.get_fields()]
+	f = field_names.value_from_object(obj)
+	name = 'Relâmpago Marquinhos'
+	context = {
+		'n': name,
+		'm': 'Mensagem',
+	}
+	# context['speed'] = a.get_speed()
+	return render(request, 'index.html', context)
     #return HttpResponse("Hello, world. You're at the bus tracker index.")
